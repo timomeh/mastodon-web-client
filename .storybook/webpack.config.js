9cac49c0cc8f8b58bd753fa593dfce52f4a17fb9
Timo Mämecke <timo@maemecke.com>
2018-09-02 14:39:26 +0200

    Change emotion rewire

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,5 @@
 const { injectBabelPlugin } = require('react-app-rewired')
-const emotionConfig = require('./emotion.config')
+const emotionConfig = require('../emotion.config')
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(['emotion', emotionConfig], config)
